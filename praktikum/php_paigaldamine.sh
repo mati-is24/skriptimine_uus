5f4e16312928e76403002671b55e879d9b3a0f65
Mati <mati.lillemaa@voco.ee>
2025-01-23 09:51:23 +0200

    töötav versioon

--- praktikum/php_paigaldamine.sh
+++ praktikum/php_paigaldamine.sh
@@ -3,14 +3,14 @@
 # kontrollime, mitu korda php7.0 korral ok installed
 # sõnad on leitud ja vastus salvestame
 # muutuja sisse:
-PHP=$(dpkg-query -W -f='${Status}' php7.0 2>/dev/null | grep -c 'ok installed')
+PHP=$(dpkg-query -W -f='${Status}' php7.4 2>/dev/null | grep -c 'ok installed')
 # kui PHP muutuja väärtus võrdub 0-ga
 if [ $PHP -eq 0  ]; then
 	# siis ok installed ei ole leitud
 	# ja väljastame vasta teate ning
 	# paigaldame teenuse
 	echo "Paigaldame php ja vajalikud lisad"
-	apt install php7.0 libapache2-mod-php7.0 php7.0-mysql
+	apt install php7.4 libapache2-mod-php7.4 php7.4-mysql
 	echo "php on paigaldatud"
 # kui PHP muutuja väärtus võrdub 1-ga
 elif [ $PHP -eq 1 ]; then
